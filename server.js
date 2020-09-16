d0ca25727fc87e315a2a44751954d96299835f2b
Joe Egan <joeegan2202@gmail.com>
2020-09-16 16:44:15 -0400

    Fixed sending connection info over LAN

--- server.js
+++ server.js
@@ -20,8 +20,8 @@ server.on('message', (msg, rinfo) => {
         let qaddress = (queue[0] !== rinfo.address) ? queue[0] : '127.0.0.1'
         let raddress = (queue[0] !== rinfo.address) ? rinfo.address : '127.0.0.1'
 
-        server.send(`${raddress}:${rinfo.port}`, queue[1], qaddress)
-        server.send(`${qaddress}:${queue[1]}`, rinfo.port, raddress)
+        server.send(`${raddress}:${rinfo.port}`, queue[1], queue[0])
+        server.send(`${qaddress}:${queue[1]}`, rinfo.port, rinfo.address)
         queue = null
     } else {
         queue = [rinfo.address, rinfo.port]
